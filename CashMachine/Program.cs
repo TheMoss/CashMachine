89c8dab7c723d910ad4c99252e201715a86da217
TheMoss <milena.k98@gmail.com>
2020-12-08 19:04:04 +0100

    Created a separate class for ATM actions

--- CashMachine/Program.cs
+++ CashMachine/Program.cs
@@ -1,49 +1,14 @@
 ﻿    using System;
-    namespace CashMachineApplication
+namespace CashMachineApplication
+{
+    class CashMachine
     {
-        class CashMachine
+        public static void Main()
         {
-            public static void Main()
-            {
-                var balance = 0;
 
-                 int DepositMoney()
-                {
-                    Console.WriteLine("How much money do you want to deposit?");
-                    var deposit = Convert.ToInt32(Console.ReadLine());
-                   return balance += deposit;
-                }
-
-                 int WithdrawMoney()
-                {
-                    Console.WriteLine("How much money do you want to withdraw?");
-                    var withdraw = Convert.ToInt32(Console.ReadLine());
-                    return balance -= withdraw;
-                }
-
-                 void CheckBalance()
-                {
-                    Console.WriteLine("Your account's balance is: {0}$", balance);
-                }
-                Console.WriteLine("To deposit money press 1 \n To withdraw money press 2 \n To check balance press 3");
-    
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1" :
-                    DepositMoney();
-                        break;
-                    case "2":
-                    WithdrawMoney();
-                        break;
-                    case "3":
-                    CheckBalance();
-                        break;
-                    default:
-                        Console.WriteLine("Invalid number");
-                        break;
-                }
-            }
+            Menu();
 
         }
     }
+
+}
