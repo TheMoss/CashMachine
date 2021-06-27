e896e78c9c97a6d57c828ce20a1e5f409b733f46
TheMoss <milena.k98@gmail.com>
2021-06-28 01:23:33 +0200

    Adds handling zero balance and splitting withdrawn money into bills

--- CashMachine/ATM.cs
+++ CashMachine/ATM.cs
@@ -4,20 +4,19 @@ namespace CashMachineApplication
 {
     public class ATM : ATMBase
     {
-        private int balance;
+        private int _balance;
         public override int Balance 
         {
-            get { return balance; }
-            set { balance = value; }
+            get; set;
         }
         public override int  Deposit(int amount)
         {
-           return balance += amount;
+           return _balance += amount;
         }
         public override int Withdraw(int amount)
         {            
-            return balance -= amount;
-        }
-        
+           return _balance -= amount;
+           
+        }       
     }
 }   
